fcd9394aaea4c35e9b9b8269c90502c2fe613281
dennismoe0 <dennismoe0@gmail.com>
2024-11-11 14:08:38 +0100

    New logic: Ingredient will no longer have the expiration date, the Fridge will when an item is added from the FoodList -> Fridge. Ingredient is only the base information of an Ingredient. When creating and adding an Ingredient to the Fridge the path would be Ingredient (Info) -> FoodList (Registry) -> Fridge (Unique instance, with date + added to totalQuantity).

--- remake/models/Ingredient.java
+++ remake/models/Ingredient.java
@@ -1,27 +1,57 @@
 package models;
 
-import remake.utilities.UnitConverter;
-import remake.utilities.DateUtility;
+import remake.utilities.UnitUtility;
+import remake.utilities.DateValidation;
 
+/**
+ * Represents an ingredient with its details such as name, category, base
+ * weight, measuring unit, and cost.
+ */
 public class Ingredient {
   private final String ingredientName;
   private final String ingredientCategory;
   private final double ingredientBaseWeight;
   private final String ingredientMeasuringUnit;
-  private double ingredientCurrentWeight; // This will be updated by the client/methods.
-  // New version stores expiration date as a long
-  private final long ingredientExpirationDate;
   private final double ingredientCost; // Make getter for cost per gram/unit.
 
+  /**
+   * Constructs a new Ingredient.
+   *
+   * @param ingredientName          the name of the ingredient
+   * @param ingredientCategory      the category of the ingredient
+   * @param ingredientBaseWeight    the base weight of the ingredient
+   * @param ingredientMeasuringUnit the measuring unit of the ingredient
+   * @param ingredientCost          the cost of the ingredient
+   */
   public Ingredient(String ingredientName, String ingredientCategory, double ingredientBaseWeight,
-      String ingredientMeasuringUnit, double ingredientCurrentWeight, long ingredientExpirationDate,
-      double ingredientCost) {
+      String ingredientMeasuringUnit, double ingredientCost) {
     this.ingredientName = ingredientName;
     this.ingredientCategory = ingredientCategory;
     this.ingredientBaseWeight = ingredientBaseWeight;
     this.ingredientMeasuringUnit = ingredientMeasuringUnit;
-    this.ingredientCurrentWeight = ingredientCurrentWeight;
-    this.ingredientExpirationDate = ingredientExpirationDate;
     this.ingredientCost = ingredientCost;
   }
+
+  // Getters
+
+  public String getIngredientName() {
+    return ingredientName;
+  }
+
+  public String getIngredientCategory() {
+    return ingredientCategory;
+  }
+
+  public double getIngredientBaseWeight() {
+    return ingredientBaseWeight;
+  }
+
+  public String getIngredientMeasuringUnit() {
+    return ingredientMeasuringUnit;
+  }
+
+  public double getIngredientCost() {
+    return ingredientCost;
+  }
+
 }
