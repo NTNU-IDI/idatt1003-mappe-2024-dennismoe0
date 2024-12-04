7b0412d21be8b9fcf5f59b3a537e9476308da51a
dennismoe0 <dennismoe0@gmail.com>
2024-12-04 15:03:07 +0100

    Created the remaining menus. Not fully working yet I believe.

--- fridgeApp/src/main/java/client/RecipeMenu.java
+++ fridgeApp/src/main/java/client/RecipeMenu.java
@@ -41,7 +41,8 @@ public class RecipeMenu {
       System.out.println("2. Manage recipes.");
       System.out.println("3. Availability menu.");
       System.out.println("4. Value menu.");
-      System.out.println("5. Back to main menu.");
+      System.out.println("5. Consume the ingredients in a recipe.");
+      System.out.println("6. Back to main menu.");
 
       int choice = scanner.nextInt();
 
@@ -52,9 +53,29 @@ public class RecipeMenu {
           recipeManager.printAllRecipes();
         }
         case 2 -> manageRecipesMenu();
-        // case 3 -> availabilityMenu();
-        // case 4 -> valueMenu();
+        case 3 -> availabilityMenu();
+        case 4 -> valueMenu();
         case 5 -> {
+          System.out.println("Generating a list of all recipes");
+          try {
+            Thread.sleep(2000);
+          } catch (InterruptedException e) {
+          }
+          recipeManager.printAllRecipes();
+
+          System.out.println("Enter the name of the recipe to consume the ingredients of:");
+          System.out.println("""
+              This will remove the ingredients from the fridge.
+              I.e. if if the recipe needs 200g of Ground Beef, it will remove this amount.""");
+
+          String recipeName = scanner.next();
+          scanner.nextLine(); // Consume the newline character
+
+          recipeManager.removeMultipleQuantitiesByRecipe(recipeName);
+          String result = recipeManager.removeMultipleQuantitiesByRecipe(recipeName);
+          System.out.println(result);
+        }
+        case 6 -> {
           return;
         }
         default -> System.out.println("Invalid choice. Please try again.");
@@ -224,11 +245,101 @@ public class RecipeMenu {
   }
 
   private void availabilityMenu() {
+    System.out.println("Availability menu:");
+    System.out.println("1. Check the fulfillment of a recipe.");
+    System.out.println("2. Get a list of all fulfilled recipes.");
+    System.out.println("3. Get a list of all partially fulfilled recipes.");
+    System.out.println("4. Return to recipe menu.");
 
+    int choice = scanner.nextInt();
+
+    switch (choice) {
+      case 1 -> {
+        System.out.println("Generating a list of all recipes");
+        try {
+          Thread.sleep(2000);
+        } catch (InterruptedException e) {
+        }
+        recipeManager.printAllRecipes();
+
+        System.out.println("Enter the name of the recipe to check fulfilment:");
+        String recipeName = scanner.next();
+        scanner.nextLine(); // Consume the newline character
+
+        recipeManager.checkIngredientsAvailability(recipeManager.getRecipeObjectByName(recipeName));
+        String result = recipeManager.checkIngredientsAvailability(
+            recipeManager.getRecipeObjectByName(recipeName));
+        System.out.println(result);
+
+      }
+      case 2 -> {
+        System.out.println("Generating a list of all fulfilled recipes");
+        try {
+          Thread.sleep(2000);
+        } catch (InterruptedException e) {
+        }
+        System.out.println(recipeManager.fullyFulfilledRecipes());
+      }
+      case 3 -> {
+        System.out.println("""
+            Generating a list of partially fulfilled recipes,
+            based on matching categories/type of ingredients or partial amounts.""");
+        try {
+          Thread.sleep(2000);
+        } catch (InterruptedException e) {
+        }
+        System.out.println(recipeManager.suggestedRecipesBasedOnFridgeContents());
+
+      }
+      case 4 -> {
+        return;
+      }
+      default -> System.out.println("Invalid choice. Please try again.");
+    }
   }
 
   private void valueMenu() {
+    System.out.println("Value menu:");
+    System.out.println("""
+        1. Get the value of a recipe.
+        Meaning the value of the quantity of each ingredient in the recipe.""");
+    System.out.println("2. Return to recipe menu.");
 
+    int choice = scanner.nextInt();
+
+    switch (choice) {
+      case 1 -> {
+        System.out.println("Generating a list of all recipes");
+        try {
+          Thread.sleep(2000);
+        } catch (InterruptedException e) {
+        }
+        recipeManager.printAllRecipes();
+
+        System.out.println("Enter the name of the recipe to get the value of:");
+        scanner.nextLine(); // Consume the newline character
+        String recipeName = scanner.nextLine();
+
+        System.out.println("Cost of the quantities in the recipe:");
+        double partialCost = recipeManager.costOfQuantitiesInRecipe(recipeName);
+        System.out.println(partialCost);
+
+        System.out.println("Total cost of the recipe:");
+        double fullCost = recipeManager.calculateRecipeCost(recipeManager.getRecipeObject(recipeName));
+        System.out.println(fullCost);
+        /**
+         * System.out.println("Recipe ingredients debugging:");
+         * Recipe recipe = recipeManager.getRecipeObjectByName(recipeName);
+         * for (String name : recipe.getIngredients().keySet()) {
+         * System.out.println(name);
+         * }
+         */
+      }
+      case 2 -> {
+        return;
+      }
+      default -> System.out.println("Invalid choice. Please try again.");
+    }
   }
 
 }
